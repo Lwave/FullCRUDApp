9c01d640394ff7da0cc32255a9c925fc01030e97
Lesley Wayne <lesleywayne97@gmail.com>
2017-02-16 14:00:49 -0600

    My full CRUD app

---
+++ Infrasctructure/EmployeeRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CRUD.Data;
+using CRUD.Models;
+
+namespace CRUD.Infrasctructure
+{
+    public class EmployeeRepository : GenericRepository<Employee>
+    {
+        EmployeeRepository(ApplicationDbContext db) : base(db)
+        {
+
+        }
+        public Employee Find(int id)
+        {
+            return (from e in _db.Employee where e.Id == id select e).FirstOrDefault();
+        }
+        public void Update(Employee emp)
+        {
+            var orig = Find(emp.Id);
+                orig.FirstName = emp.FirstName;
+                orig.LastName = emp.LastName;
+                orig.Email = emp.Email;
+                orig.SSN = emp.SSN;
+                orig.HourlyWage = emp.HourlyWage;
+                orig.Benefits = emp.Benefits;
+            }
+        }
+    }
+}
---
+++ Services/EmployeeService.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CRUD.Infrasctructure;
+using CRUD.Models;
+
+namespace CRUD.Services
+{
+    public class EmployeeService
+    {
+        private EmployeeRepository _repo;
+
+        public EmployeeService(EmployeeRepository repo)
+        {
+            _repo = repo;
+        }
+      public IList<Employee> GetEmployees()
+        {
+            return _repo.List().ToList();
+        }
+
+        public Employee FindEmployee(int id)
+        {
+            return _repo.Find(id);
+        }
+
+        public IList<MinEmployee> GetMinifiedEmployees()
+        {
+            var employees = _repo.List();
+            return (from e in employees
+                    select new MinEmployee
+                    {
+                        Id = e.Id,
+                        FirstName = e.FirstName,
+                        LastName = e.LastName,
+                        Email = e.Email
+
+                    }).ToList();
+        }
+
+        public void AddEmployee(Employee emp)
+        {
+            _repo.Add(emp);
+            _repo.SaveChanges();
+        }
+
+        public void UpdateEmployees(Employee emp)
+        {
+            _repo.Update(emp);
+            _repo.SaveChanges();
+            
+        }
+
+        public void DeleteEmployee(int id)
+        {
+            var emp = _repo.Find(id);
+            _repo.Delete(emp);
+            _repo.SaveChanges();
+        }
+
+    }
+}
